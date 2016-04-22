e1b1a98a16e3ffab54052ce479a65511d8d37d22
yibuyisheng <yibuyisheng@163.com>
2016-04-22 20:09:44 +0800

    push pull

--- src/Grid/Column.js
+++ src/Grid/Column.js
@@ -33,9 +33,27 @@ export default class Column extends Component {
 
         const columns = parseInt(this.props.columns, 10);
         if (!columns || columns > 12 || columns < 1) {
-            throw new Error(`Column: wrong columns attribute, must between 1 and 12`);
+            throw new Error(`Column: wrong columns attribute, must be between 1 and 12`);
         }
         cssClass += '-' + columns;
+
+        const pull = parseInt(this.props.pull, 10);
+        const push = parseInt(this.props.push, 10);
+        if (pull > 12 || pull < 1) {
+            throw new Error('wrong `pull` attribute: ' + pull);
+        }
+        if (push > 12 || push < 1) {
+            throw new Error('wrong `push` attribute: ' + push);
+        }
+        if (pull && push) {
+            throw new Error(`the \`pull\` property can not be used with \`push\` property.`);
+        }
+        else if (pull) {
+            cssClass += ` col-${this.props.size}-pull-${pull}`;
+        }
+        else if (push) {
+            cssClass += ` col-${this.props.size}-push-${push}`;
+        }
 
         this.setState({cssClass});
     }
